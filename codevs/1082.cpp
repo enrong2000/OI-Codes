273e786db0184d9b16aa304c44bd0f893a0dc8f6
enrong2000 <zerxli@outlook.com>
2016-11-14 21:06:50 +0800

    commit via push.cmd

---
+++ codevs/1082.cpp
@@ -0,0 +1,142 @@
+//Self - Writing XGYZ WDMC SCFY
+//CODEVS 1081
+//Time Limit : 1000 MS
+//Memory Limit : 128 MB
+//LDNU IPWK - Enrong
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+#define INF 0x3f3f3f3f
+#define N 100010
+#define lson ((id)<<1)
+#define rson ((id)<<1|1)
+#define mid  ((l+r)>>1)
+int A[N], Add[N << 2], Sum[N << 2];
+int n, i, ask, cmd, xx, yy, va, ans;
+#define int long long
+
+int in()
+{
+	int ch = getchar(), x = 0, f = 1;
+	while (ch < '0' || ch > '9')
+	{
+		if (ch == '-')
+			f = -1;
+		ch = getchar();
+	}
+	while (ch >= '0' && ch <= '9')
+	{
+		x = x * 10 + ch - '0';
+		ch = getchar();
+	}
+	return (x * f);
+}
+
+#undef int
+
+void PushUp(int id)
+{
+	Sum[id] = Sum[lson] + Sum[rson];
+}
+
+void PushDown(int id, int ln, int rn)
+{
+	if (Add[id])
+	{
+		Add[lson] += Add[id];
+		Add[rson] += Add[id];
+		Sum[lson] += Add[id] * ln;
+		Sum[rson] += Add[id] * rn;
+		Add[id] = 0;
+	}
+}
+
+void Build(int l, int r, int id)
+{
+	if (l == r)
+	{
+		Sum[id] = A[l];
+		return;
+	}
+	Build(l, mid, lson);
+	Build(mid + 1, r, rson);
+	PushUp(id);
+}
+
+void Update1(int L, int val, int l, int r, int id)
+{
+	if (l == r)
+	{
+		Sum[id] += val;
+		return;
+	}
+	if (L <= mid) Update1(L, val, l, mid, lson);
+	if (L > mid) Update1(L, val, mid + 1, r, rson);
+	PushUp(id);
+}
+
+void Update2(int L, int R, int val, int l, int r, int id)
+{
+	if (L <= l && r <= R)
+	{
+		Sum[id] += val * (r - l + 1);
+		Add[id] = val;
+		return;
+	}
+	PushDown(id, mid - l + 1, r - mid);
+	if (L <= mid) Update2(L, R, val, l, mid, lson);
+	if (R > mid) Update2(L, R, val, mid + 1, r, rson);
+	PushUp(id);
+}
+
+int Query(int L, int R, int l, int r, int id)
+{
+	if (L <= l && r <= R)
+	{
+		return Sum[id];
+	}
+	PushDown(id, mid - l + 1, r - mid);
+	int ANS = 0;
+	if (L <= mid) ANS += Query(L, R, l, mid, lson);
+	if (R > mid) ANS += Query(L, R, mid + 1, r, rson);
+	PushUp(id);
+	return ANS;
+}
+
+int main()
+{
+	n = in();
+	for (int j = 1; j <= n; ++j)
+		A[j] = in();
+	Build(1, n, 1);
+	ask = in();
+	for (int k = 0; k < ask; ++k)
+	{
+		cmd = in();
+		if (cmd == 1)
+		{
+			xx = in(), yy = in(), va = in();
+			Update2(xx, yy, va, 1, n, 1);
+		}
+		if (cmd == 2)
+		{
+			xx = in(), yy = in();
+			ans=Query(xx,yy,1,n,1);
+			printf("%d\n", ans);
+		}
+	}
+	return 0;
+}
